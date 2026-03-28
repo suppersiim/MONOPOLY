17fdd9ae4a9314934ce1ab722ce7dd52765e6ee6
mikkjavois <mikk.javois@gmail.com>
2026-03-28 10:31:55 +0200

    Migrate to maven

---
+++ src/main/java/common/PacketType.java
@@ -0,0 +1,22 @@
+package common;
+
+public enum PacketType {
+
+    // client packets
+    CLIENT_TEST,
+
+    // server packets
+    SERVER_GAME_STATE_UPDATE,
+
+    // general packets
+    QUIT;
+
+
+    public static PacketType fromInt(int value) {
+        return values()[value];
+    }
+
+    public int toInt() {
+        return ordinal();
+    }
+}
---
+++ src/main/java/server/Main.java
@@ -0,0 +1,8 @@
+package server;
+
+public class Main {
+    static void main() {
+        GameServer server = new GameServer(8080);
+        server.start();
+    }
+}
